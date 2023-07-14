8abbc9688779c3a4ac030dd9aec4d6af9166be72
laurekt <laurenvien2006@gmail.com>
2023-07-14 09:30:29 -0700

    Initial Commit

---
+++ navStack/navStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  navStackApp.swift
+//  navStack
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct navStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
